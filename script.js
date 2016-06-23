8c103bdc703ad192f9620a6e589f866b9d8777d4
Kyle Johnson <joh04667@gmail.com>
2016-06-22 19:49:39 -0500

    adds quantity amount and increase/decrease functionality

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 var viewModel = {
 	newItemName: ko.observable(),
 	newItemPrice: ko.observable(0),
+	newItemQuantity: ko.observable(1),
 	addNewItem: function () {
 		var newItem = {
 			name: this.newItemName(),
-			price: this.newItemPrice()
+			price: this.newItemPrice(),
+			quantity: ko.observable(this.newItemQuantity())
 		};
 
 		this.itemsInCart.push(newItem);
 		this.newItemName("");
 		this.newItemPrice(0);
+		this.newItemQuantity(1);
 	},
+
+	incrementUp: function() {
+		var value = this.quantity();
+		value++;
+		this.quantity(value);
+	},
+
+	incrementDown: function() {
+		var value = this.quantity();
+		// don't update value if decremented value is zero or lower
+		value = value - 1 ? value - 1 : value;
+		this.quantity(value);
+	},
+
 	removeItem: function() {
 		viewModel.itemsInCart.remove(this);
 	},
+
 	itemsInCart: ko.observableArray([])
 
 };
 
-// return false if either field left blank
 viewModel.addNewItemEnabled = ko.pureComputed(function() {
 	var name = this.newItemName(),
 		price = this.newItemPrice();
 	return name && name.length;
 }, viewModel);
+
+
 
 ko.applyBindings(viewModel);
